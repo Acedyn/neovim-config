83ca05c997bdb257d142adadc35af5619bec423b
Simon Lambin <slambin@artfx.fr>
2022-01-16 00:53:36 +0100

    Disable formatting of tsserver

--- modules/lsp-installer.lua
+++ modules/lsp-installer.lua
@@ -8,6 +8,12 @@ email: simo.lambin@gmail.com
 -- Register a handler that will be called for all installed servers.
 require("nvim-lsp-installer").on_server_ready(function(server)
 	local opts = {}
+    opts.on_attach = function(client, bufnr) 
+        -- Disable formatting of tsserver
+        if client.name == "tsserver" then
+            client.resolved_capabilities.document_formatting = false
+        end
+    end
 
 	server:setup(opts)
 end)
